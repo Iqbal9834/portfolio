ac3535aa1b6a83e191cf6ab17bcd99aab3126373
Manish Sharma <manishsharma959939@gmail.com>
2021-10-24 11:57:27 +0530

    updates img alt text

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,36 +1,47 @@
-import React from 'react';
-import Style from './Portfolio.module.scss';
-import mock1 from '../img/mock1.webp';
-import mock2 from '../img/mock2.jpeg';
-import mock3 from '../img/mock3.jpeg';
-import {info, colors} from '../Info/Info';
-
+import React from "react";
+import Style from "./Portfolio.module.scss";
+import mock1 from "../img/mock1.webp";
+import mock2 from "../img/mock2.jpeg";
+import mock3 from "../img/mock3.jpeg";
+import { info, colors } from "../Info/Info";
 
 export default function Portfolio() {
-    let portfolioInfo = info.portfolio;
+  let portfolioInfo = info.portfolio;
 
-    let mocks = [mock1, mock2, mock3];
+  let mocks = [mock1, mock2, mock3];
 
-    function createGrid() {
-        let results = [];
-        let j = 0;
-        for(let i = 0; i < portfolioInfo.length; i++) {
-                results.push(
-                    <div className={Style.block}>
-                        <a target="_blank" rel="noopener noreferrer" href={info.portfolio[i].liveLink}><img className={Style.image} src={mocks[j++]} alt='image'/></a>
-                        <h1>{info.portfolio[i].title}</h1>
-                        <a target="_blank" rel="noopener noreferrer" href={info.portfolio[i].sourceLink} style={{backgroundColor: colors[1]}} className={Style.source}>Source Code</a>
-                    </div>
-                )
-        }
-        return results;
+  function createGrid() {
+    let results = [];
+    let j = 0;
+    for (let i = 0; i < portfolioInfo.length; i++) {
+      results.push(
+        <div className={Style.block}>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={info.portfolio[i].liveLink}
+          >
+            <img className={Style.image} src={mocks[j++]} alt="user-photo" />
+          </a>
+          <h1>{info.portfolio[i].title}</h1>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={info.portfolio[i].sourceLink}
+            style={{ backgroundColor: colors[1] }}
+            className={Style.source}
+          >
+            Source Code
+          </a>
+        </div>
+      );
     }
+    return results;
+  }
 
-    return (
-       <main>
-           <div className={Style.projectsGrid}>
-               {createGrid()}
-           </div>
-       </main>
-    );
-};
+  return (
+    <main>
+      <div className={Style.projectsGrid}>{createGrid()}</div>
+    </main>
+  );
+}
